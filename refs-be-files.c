a69486a32fec358e02f45b24437b060b41bd585a
Ronnie Sahlberg <sahlberg@google.com>
2014-11-03 11:53:27 -0800

    refs.c: move delete_ref to the common code
    This change moves delete_ref() to the refs.c file since this function
    does not contain any backend specific code.
    
    Based on a patch by Ronnie Sahlberg.
    
    Signed-off-by: David Turner <dturner@twitter.com>
    Signed-off-by: Ronnie Sahlberg <sahlberg@google.com>

--- refs-be-files.c
+++ refs-be-files.c
@@ -2686,27 +2686,6 @@ static int delete_ref_loose(struct ref_lock *lock, int flag, struct strbuf *err)
 	return 0;
 }
 
-int delete_ref(const char *refname, const unsigned char *sha1, unsigned int flags)
-{
-	struct ref_transaction *transaction;
-	struct strbuf err = STRBUF_INIT;
-
-	transaction = ref_transaction_begin(&err);
-	if (!transaction ||
-	    ref_transaction_delete(transaction, refname,
-				   (sha1 && !is_null_sha1(sha1)) ? sha1 : NULL,
-				   flags, NULL, &err) ||
-	    ref_transaction_commit(transaction, &err)) {
-		error("%s", err.buf);
-		ref_transaction_free(transaction);
-		strbuf_release(&err);
-		return 1;
-	}
-	ref_transaction_free(transaction);
-	strbuf_release(&err);
-	return 0;
-}
-
 /*
  * People using contrib's git-new-workdir have .git/logs/refs ->
  * /some/other/path/.git/logs/refs, and that may live on another device.
