6e57cbffd1588763217cb1e7f9fe68ec470717d0
Ronnie Sahlberg <sahlberg@google.com>
2014-11-03 13:06:39 -0800

    refs.c: move resolve_refdup to common
    This function can be shared across all refs backends so move it
    to the common code.
    
    Signed-off-by: Ronnie Sahlberg <sahlberg@google.com>
    Signed-off-by: David Turner <dturner@twitter.com>

--- refs-be-files.c
+++ refs-be-files.c
@@ -1643,11 +1643,6 @@ const char *resolve_ref_unsafe(const char *refname, int resolve_flags, unsigned 
 	}
 }
 
-char *resolve_refdup(const char *ref, int resolve_flags, unsigned char *sha1, int *flags)
-{
-	return xstrdup_or_null(resolve_ref_unsafe(ref, resolve_flags, sha1, flags));
-}
-
 /* The argument to filter_refs */
 struct ref_filter {
 	const char *pattern;
