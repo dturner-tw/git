4ee72ae55fdbd52d6c32d20e9f170e4892a25d2c
David Turner <dturner@twopensource.com>
2015-06-16 18:51:53 -0400

    refs-be-files.c: add method to expire reflogs
    Signed-off-by: David Turner <dturner@twopensource.com>

--- refs.c
+++ refs.c
@@ -1076,3 +1076,15 @@ int bulk_update_commit(void)
 {
 	return the_refs_backend->bulk_update_commit();
 }
+
+int reflog_expire(const char *refname, const unsigned char *sha1,
+		  unsigned int flags,
+		  reflog_expiry_prepare_fn prepare_fn,
+		  reflog_expiry_should_prune_fn should_prune_fn,
+		  reflog_expiry_cleanup_fn cleanup_fn,
+		  void *policy_cb_data)
+{
+	return the_refs_backend->reflog_expire(refname, sha1, flags,
+					       prepare_fn, should_prune_fn,
+					       cleanup_fn, policy_cb_data);
+}
